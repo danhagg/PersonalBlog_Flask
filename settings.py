6be55ad4f89320cee030b1bb38745cf1fd4893a3
danhagg <danielhaggerty1976@gmail.com>
2017-11-06 14:41:20 -0600

    settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 DB_USERNAME = 'root'
 DB_PASSWORD = ''
 BLOG_DATABASE_NAME = 'blog'
-DB_HOST = os.getenv('IP', '0.0.0.0')
+DB_HOST = os.getenv('localhost')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,
                                           BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
