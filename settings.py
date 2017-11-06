ec323f661d5a8595a5e1e647996b65e95cbf264d
danhagg <danielhaggerty1976@gmail.com>
2017-11-06 15:42:55 -0600

    db setup

--- settings.py
+++ settings.py
@@ -5,9 +5,14 @@ DEBUG = True
 DB_USERNAME = 'danhagg'
 DB_PASSWORD = 'Pilky01'
 BLOG_DATABASE_NAME = 'blog'
-DB_HOST = os.getenv('localhost')
-DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,
-                                          BLOG_DATABASE_NAME)
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
+if DATABASE_URL:
+    DB_URI = "mysql+py" + DATABASE_URL
+
+else:
+    DB_HOST = os.getenv('localhost')
+    DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD,
+                                              DB_HOST, BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOADED_IMAGES_DEST = '/opt/flask_blog/static/images'
