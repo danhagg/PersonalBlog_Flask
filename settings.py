65f526c3f138e0dd41485470fa280373f6c3f75f
danhagg <danielhaggerty1976@gmail.com>
2017-11-06 14:43:32 -0600

    settings.py

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 SECRET_KEY = 'you-will-never-guess'
 DEBUG = True
-DB_USERNAME = 'root'
-DB_PASSWORD = ''
+DB_USERNAME = 'danhagg'
+DB_PASSWORD = 'Pilky01'
 BLOG_DATABASE_NAME = 'blog'
 DB_HOST = os.getenv('localhost')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,
