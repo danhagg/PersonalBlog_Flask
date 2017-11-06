636003b051f8726b3e4b7a279c351bbde04c6774
danhagg <danielhaggerty1976@gmail.com>
2017-11-06 13:44:51 -0600

    settings

--- settings.py
+++ settings.py
@@ -5,8 +5,7 @@ DEBUG = True
 DB_USERNAME = 'root'
 DB_PASSWORD = ''
 BLOG_DATABASE_NAME = 'blog'
-#DB_HOST = 'mysql:3306'
-DB_HOST = os.getenv('localhost')
+DB_HOST = os.getenv('IP', '0.0.0.0')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,
                                           BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
@@ -15,6 +14,3 @@ UPLOADED_IMAGES_DEST = '/opt/flask_blog/static/images'
 #UPLOADED_IMAGES_DEST = '/home/ubuntu/workspace/flask_blog/static/images'
 UPLOADED_IMAGES_DEST = '/flask_blog/static/images'
 UPLOADED_IMAGES_URL = '/static/images/'
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
