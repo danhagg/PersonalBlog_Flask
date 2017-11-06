241e5952acee3119dac245f7c6bd9631663d78a0
danhagg <danielhaggerty1976@gmail.com>
2017-11-06 13:31:46 -0600

    wsgi and settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 DB_USERNAME = 'root'
 DB_PASSWORD = ''
 BLOG_DATABASE_NAME = 'blog'
-# DB_HOST = 'mysql:3306'
+#DB_HOST = 'mysql:3306'
 DB_HOST = os.getenv('localhost')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,
                                           BLOG_DATABASE_NAME)
@@ -13,7 +13,7 @@ SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOADED_IMAGES_DEST = '/opt/flask_blog/static/images'
 #UPLOADED_IMAGES_DEST = '/home/ubuntu/workspace/flask_blog/static/images'
-#UPLOADED_IMAGES_DEST = '/flask_blog/static/images'
+UPLOADED_IMAGES_DEST = '/flask_blog/static/images'
 UPLOADED_IMAGES_URL = '/static/images/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,19 @@
+"""
+Gunicorn takes a flag, --chdir, that lets you select which directory your Python app lives in. So, if you have a directory structure like:
+
+my-project/
+  Procfile
+  my_folder/
+    my_module.py
+and my_module.py contains:
+
+app = Flask(__name__, ...)
+You can put the following in your Procfile:
+
+web: gunicorn --chdir my_folder my_module:app
+my app = Flask(__name__, ...) is in __init__.py
+"""
+
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
